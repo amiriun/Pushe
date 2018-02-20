acdf0daa0ecbe84dd6a9ec921bb9df164e4562a9
Amir Alian <amircup2006@gmail.com>
2018-02-20 13:48:10 +0330

    Add manager for push

---
+++ src/PushManager.php
@@ -0,0 +1,46 @@
+<?php
+namespace Amiriun\Pushe;
+
+class PushManager{
+
+    public $title;
+    public $content;
+    public $sound;
+    public $rawData;
+
+    public function send(){
+
+    }
+
+    public function filterByUsers($users){
+
+    }
+
+    public function setTitle(){
+
+    }
+
+    public function getTitle(){
+
+    }
+
+    public function setContent(){
+
+    }
+
+    public function getContent(){
+
+    }
+
+    public function setSound(){
+
+    }
+
+    public function getSound(){
+
+    }
+
+    public function setRawData(){
+
+    }
+}
